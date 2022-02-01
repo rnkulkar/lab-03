81b3217ed540df06083235e260ad4b90c7d867b8
rnkulkar <rnkulkar@ucsc.edu>
2022-02-01 14:48:31 -0800

    Completed Problems 6-11

--- lab-03_script.R
+++ lab-03_script.R
@@ -21,3 +21,12 @@ ggplot(data=elmhurst, aes(x=family_income, y=gift_aid) ) + geom_point() +
 model <- lm(gift_aid ~ family_income, data = elmhurst)
 tidy(model) %>%
   kable(digits = 3)
+
+elmhurst <- elmhurst %>%
+  mutate(resid = residuals(model))
+
+ggplot(data=elmhurst, aes(x=family_income, y=resid) ) + geom_point() + 
+  xlab('Family Income in thousands') + ylab('Residuals') + 
+  ggtitle("Residuals of LR on Family Income and Gift Aid")
+
+hist(elmhurst$resid)
